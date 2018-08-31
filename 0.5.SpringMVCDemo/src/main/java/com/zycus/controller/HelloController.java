c8fe3bab94c0a3ca59624ef32daa9cbc2578fb4e
Sonali Gupta <guptasonali6688@outlook.com>
2018-08-31 18:22:01 +0530

    Add files via upload

---
+++ 0.5.SpringMVCDemo/src/main/java/com/zycus/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.zycus.controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/hello.do")
+	public String hello(Map model) {
+		model.put("message", "Welcome to SpringMVC");
+		return "hello.jsp"; //view
+	}
+}
